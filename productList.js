38d495311c254900da623a70f1986d4900effd52
thordur <thordur921@gmail.com>
2022-05-02 11:35:48 +0200

    cards v2, missing color function

--- productList.js
+++ productList.js
@@ -11,15 +11,12 @@ function getProducts(){
 function setupProducts(products){
     const template = document.querySelector("template").content;
     const parentElement = document.querySelector(".productList");
-    const productDiv = document.querySelector(".product");
 
     products.forEach(product=>{
         const copy = template.cloneNode(true);
-        copy.querySelector("img").src = product.list_image1;
-        
-        
-        
-        
+        copy.querySelector(".productImage").src = product.list_image1;
+        copy.querySelector(".productImage2").src = product.list_image2;
+
         copy.querySelector(".cardTitle").textContent = product.title.rendered;
         copy.querySelector(".cardPrice").textContent = `DKK ${product.price}`;
         
